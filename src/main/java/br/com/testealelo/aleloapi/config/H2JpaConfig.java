e98cc2cb65d3d31dbc1b217ff71e423642f5e47d
enzocouto <enzocouto@gmail.com>
2021-04-28 22:07:45 -0300

    properties h2

--- src/main/java/br/com/testealelo/aleloapi/config/H2JpaConfig.java
+++ src/main/java/br/com/testealelo/aleloapi/config/H2JpaConfig.java
@@ -4,19 +4,15 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import br.com.testealelo.aleloapi.repository.VehicleRepository;
-
-//@Configuration
-//@EnableJpaRepositories(basePackages = "br.com.testealelo.aleloapi.repository")
-//@EnableTransactionManagement
+@Configuration
+@EnableJpaRepositories(basePackages = "br.com.testealelo.aleloapi.repository")
+@EnableTransactionManagement
 public class H2JpaConfig {
 	
 	@Autowired
@@ -25,10 +21,10 @@ public class H2JpaConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-        dataSource.setUrl(env.getProperty("jdbc.url"));
-        dataSource.setUsername(env.getProperty("jdbc.user"));
-        dataSource.setPassword(env.getProperty("jdbc.pass"));
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driverClassName"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
 
         return dataSource;
     }
